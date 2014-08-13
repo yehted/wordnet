6157763932ec67894af96cf1569991bfbfbb1450
yehted <yeh.ted@gmail.com>
2014-08-13 11:13:33 -0500

    made main program more user friendly

--- main.cpp
+++ main.cpp
@@ -12,56 +12,64 @@ using namespace std;
 // SAP test
 void SAP_test() {
 	cout << "-- SAP test --" << endl;
+	cout << "Enter digraph filename: ";
+	string file;
+	cin >> file;
 	ifstream inFile;
-	inFile.open("files/digraph1.txt");
-	//	inFile.open(argv[1]);
+	inFile.open(file);
+//	inFile.open("files/digraph1.txt");
 	if (!inFile.is_open()) {
 		cerr << "File not opened!" << endl;
-		exit(1);
+		return;
 	}
 	Digraph G(inFile);
 	cout << G << endl;
 	SAP sap(G);
 	int v, w;
-	cout << "v: ";
-	cin >> v;
-	cout << "w: ";
-	cin >> w;
+	cout << "v: (-1 to exit)";
+	while (cin >> v) {
+		if (v == -1) break;
 
-	int length = sap.length(v, w);
-	int ancestor = sap.ancestor(v, w);
-	printf("length = %d, ancestor = %d\n", length, ancestor);
+		cout << "w: ";
+		cin >> w;
 
+		int length = sap.length(v, w);
+		int ancestor = sap.ancestor(v, w);
+		printf("length = %d, ancestor = %d\n", length, ancestor);
+		cout << "v: ";
+	}
 }
 
 // WordNet test
-void WordNet_test() {
+void WordNet_test(WordNet& word) {
 	cout << "-- WordNet test --" << endl;
-	string synsetsFile = "files/synsets.txt";
-	string hypernymsFile = "files/hypernyms.txt";
-
-	WordNet word(synsetsFile, hypernymsFile);
+	
 	string a, b;
-	for (int i = 0; i < 3; i++) {
-		cout << "First word: ";
-		cin >> a;
+	cout << "Enter words (-1 to quit)" << endl;
+	cout << "First word: ";
+	while (cin >> a) {
+		if (a == "-1") break;
+
 		cout << "Second word: ";
 		cin >> b;
+		if (!word.isNoun(a) || !word.isNoun(b)) {
+			cerr << "Word(s) not found" << endl;
+			return;
+		}
 		//a = "Black_Plague";
 		//b = "black_marlin";
 		int d = word.distance(a, b);
 		string anc = word.sap(a, b);
 		cout << d << " = distance between " << a << " and " << b << endl;
 		cout << anc << " = common ancestor between " << a << " and " << b << endl;
+		cout << "First word: ";
 	}
 }
 
 // Outcast test
-void Outcast_test() {
+void Outcast_test(WordNet& wordnet) {
 	cout << "-- Outcast test --" << endl;
-	string synsetsFile = "files/synsets.txt";
-	string hypernymsFile = "files/hypernyms.txt";
-	WordNet wordnet(synsetsFile, hypernymsFile);
+	
 	Outcast outcast(wordnet);
 	string file;
 	cout << "Filename: (exit to quit)";
@@ -73,7 +81,7 @@ void Outcast_test() {
 		//	inFile.open(argv[1]);
 		if (!inFile.is_open()) {
 			cerr << file << " not opened!" << endl;
-			exit(1);
+			return;
 		}
 		string buffer;
 		int N = 0;
@@ -109,8 +117,30 @@ void Outcast_test() {
 }
 
 int main(int argc, char* argv[]) {
-//	SAP_test();
-//	WordNet_test();
-	Outcast_test();
+	// Loads in the synsets and hypernyms file
+	string synsetsFile = "files/synsets.txt";
+	string hypernymsFile = "files/hypernyms.txt";
+	WordNet wordnet(synsetsFile, hypernymsFile);
+
+	cout << "** WordNet **" << endl;
+	cout << "Please choose one of the following: " << endl;
+	cout << "1. SAP test" << endl << "2. WordNet test" << endl << "3. Outcast test" << endl;
+	int choice;
+	cin >> choice;
+	switch (choice) {
+	case 1:
+		SAP_test();
+		break;
+	case 2:
+		WordNet_test(wordnet); 
+		break;
+	case 3:
+		Outcast_test(wordnet);
+		break;
+	default:
+		cout << "Invalid input" << endl;
+	}
+	cout << "Done!" << endl;
+
 	return 0;
 }
